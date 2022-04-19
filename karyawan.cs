309f4d3f000c5e608e1b4ef713eda5c28b7622c9
retianayunitayeyen <retianayunitayeyen@students.amikom.ac.id>
2022-04-19 11:50:39 +0700

    inisialisasi project

---
+++ karyawan.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class karyawan
+    {
+    }
+}
+public class main { }
+       public static void main(string{ }
+args) {
+      scanner input = new
+scanner(System.in);
+
+        interface jamkerja ,jumlah ,golongan, lembur, lamkerja, gajipokok , jamlembur :
+                  float gajibersih;
+string namakaryawan;
+
+       system .out.print("/n/tMasukan Nama : ");
+       namakaryawan=input .nex();
+
+    system .out.print("\tMasukan jam kerja  :  ");
+         jamkerja =input.nexInt();
+
+     
+        if (jamkerja>135){
+            jamlembur=jamkerja -135;
+    }else{
+        jamlembur =0;
+    }
+
+    swicth(golonga)
+{
+    lembur = jamlembur * 20000;
+    gajibersih =
